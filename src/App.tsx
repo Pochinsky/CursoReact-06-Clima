b8e82b33519e2dcf4530faaf659ade93bd780d4b
Tomás Guttman P. <tguttman@oticosofofa.cl>
2024-12-16 05:27:52 -0300

    🎉 project initializated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import style from "./App.module.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className={style.title}>Consulte el clima</h1>
+      <div className={style.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  );
+}
