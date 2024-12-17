5e797726e001193d3d402f6be795a20b95a83fd7
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-12-17 15:56:48 -0300

    🔖 project finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import style from "./App.module.css";
+import styles from "./App.module.css";
+import Alert from "./components/Alert/Alert";
+import Form from "./components/Form/Form";
+import Spinner from "./components/Spinner/Spinner";
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
+import useWeather from "./hooks/useWeather";
 
 export default function App() {
+  const { loading, weather, fetchWeather, hasWeatherData, notFound } =
+    useWeather();
   return (
     <>
-      <h1 className={style.title}>Consulte el clima</h1>
-      <div className={style.container}>
-        <p>1</p>
-        <p>2</p>
+      <h1 className={styles.title}>Consulte el clima</h1>
+      <div className={styles.container}>
+        <Form fetchWeather={fetchWeather} />
+        {loading && <Spinner />}
+        {hasWeatherData && !loading && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>Ciudad no encontrada</Alert>}
       </div>
     </>
   );
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,64 @@
+import { useMemo, useState } from "react";
+import axios from "axios";
+import { z } from "zod";
+import type { Search } from "../types";
+
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+
+export type Weather = z.infer<typeof Weather>;
+
+const initialState: Weather = {
+  name: "",
+  main: {
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+  },
+};
+
+export default function useWeather() {
+  // States
+  const [weather, setWeather] = useState<Weather>(initialState);
+  const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+
+  // API
+  const apiKey: string = import.meta.env.VITE_API_KEY;
+
+  const fetchWeather = async (search: Search) => {
+    setLoading(true);
+    setWeather(initialState);
+    try {
+      // Get geo data
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`;
+      const { data } = await axios.get(geoUrl);
+      // Validate that country exists
+      if (!data[0]) {
+        setNotFound(true);
+        return;
+      }
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      // Get weather data
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      const { data: weatherResult } = await axios.get(weatherUrl);
+      const result = Weather.safeParse(weatherResult);
+      if (result.success) setWeather(result.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const hasWeatherData = useMemo(() => weather.name, [weather]);
+
+  return { weather, loading, notFound, hasWeatherData, fetchWeather };
+}
